76e8572aa30092f156cec194cfc7be9abc5558b5
arnaudlemaire <arnaud@geekylab.com>
2020-05-19 13:28:10 +0200

    add root and healthcheck endpoints

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    const sleepTime = process.env.SLEEP_TIME;
-    await sleep(sleepTime);
-  }
-}
+const express = require("express");
+const app = express();
 
-main();
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+app.get("/", ( req, res ) => {
+  res.status(200).send("Hello World!");
+});
+
+app.get("/healthcheck", ( req, res ) => {
+  res.status(200).send("Healthcheck OK!");
+});
